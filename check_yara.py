b814ed10b97063acd51fbf8af9658b3320f35a5a
Denis Legezo <dlegezo@gmail.com>
2024-02-28 14:33:33 +0400

    Pass access denied errors

--- check_yara.py
+++ check_yara.py
@@ -2,12 +2,13 @@
 import pyfanotify
 import select
 import yara
+import os
 
 def subscribe_exec():
     notifier = pyfanotify.Fanotify()
-    notifier.mark("/home/dlegezo")
+    notifier.mark("/", is_type="fs")
     notifier.start()
-    client = pyfanotify.FanotifyClient(notifier, path_pattern='/home/*')
+    client = pyfanotify.FanotifyClient(notifier, path_pattern='*')
 
     poller = select.poll()
     poller.register(client.sock.fileno(), select.POLLIN)
@@ -16,12 +17,15 @@ def subscribe_exec():
             desc = {}
             for event in client.get_events():
                 # 4129 - access|open|open_exec, 33 - access|open to add .so checks
-                if (event.ev_types == 4129 or event.ev_types == 33):
+                if (event.ev_types == 4129):
+                    # check_file_permissions(str(event.path)[2:-1])
                     check_yara(str(event.path), event.pid) 
                 event.ev_types = pyfanotify.evt_to_str(event.ev_types)
                 desc.setdefault(event.path, []).append(event)
-    except:
-        print('monitoring stopped, exception occured')
+    except PermissionError:
+        print("Access denied", event.ev_types)
+    finally:
+        print("Monitoring stopped due to exception")
 
     client.close()
     notifier.stop()
@@ -35,8 +39,11 @@ def malware_handler_disk(data):
 def check_yara(path: str, pid: int):
     print("checking", path, pid)
     rules = yara.compile("/home/dlegezo/poseidon.yara")
-    matches = rules.match(pid=pid, callback=malware_handler_mem, which_callbacks=yara.CALLBACK_MATCHES)
-    matches = rules.match(path[2:-1], callback=malware_handler_disk, which_callbacks=yara.CALLBACK_MATCHES)
+    try:
+        matches = rules.match(pid=pid, callback=malware_handler_mem, which_callbacks=yara.CALLBACK_MATCHES)
+        matches = rules.match(path[2:-1], callback=malware_handler_disk, which_callbacks=yara.CALLBACK_MATCHES)
+    except yara.Error as e:
+        print("Yara error", e) 
 
 def main():
     subscribe_exec()    
